d5ef66f0a499e6e76b212fa9035d2ef047576255
deyantodorov <deyan.todorov@hotmail.com>
2015-11-13 16:23:03 +0200

    Combinatorial homework

---
+++ Data-Structures-Algorithms/HW8.Combinatorics/BinaryPasswords/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BinaryPasswords
+{
+    using System;
+
+    /// <summary>
+    /// 01. Двоични пароли
+    /// http://bgcoder.com/Contests/Practice/Index/132#0
+    /// </summary>
+    public class Program
+    {
+        private static void Main()
+        {
+            var passwordTemplate = Console.ReadLine();
+            int unknownDigitsNumber = 0;
+
+            for (int i = 0; i < passwordTemplate.Length; i++)
+            {
+                if (passwordTemplate[i] == '*')
+                {
+                    unknownDigitsNumber++;
+                }
+            }
+
+            long answer = 1;
+
+            for (int i = 1; i <= unknownDigitsNumber; i++)
+            {
+                answer *= 2;
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
